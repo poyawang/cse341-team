66fcf5dbc028db2e8aa3fc85ee24e4d3439bbd13
poyawang <poyawang12@gmail.com>
2022-01-13 11:17:28 -0700

    week2

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,42 @@
 const express = require('express');
 const router = express.Router();
 
+let users = [];
+let userDis = true;
+let addUserDis = true;
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  // const users = users;
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
+    usrs : users,
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    userDis: userDis, 
+    addUserDis: addUserDis
   });
+});
+
+// /ta02/addUser => POST
+router.post('/addUser', (req, res, next) => {
+  addUserDis = users.includes(req.body.user)
+  if (addUserDis == false) {
+    users.push( req.body.user);
+    console.log(users)
+  }
+  
+  res.redirect('/ta02');
+});
+
+// /ta02/removeUser => POST
+router.post('/removeUser', (req, res, next) => {
+  users = users.filter((name) => name !=req.body.removeUser )
+  userDis = users.includes(req.body.removeUser)
+  console.log(users)
+  console.log(userDis)
+
+  res.redirect('/ta02');
 });
 
 module.exports = router;
